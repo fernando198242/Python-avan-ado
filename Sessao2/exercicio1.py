65fe144d470ce0a883dc1d6bb5e4d245b110d1ce
fernando198242 <nandobatista22@hotmail.com>
2025-09-18 20:02:03 +0100

    Atualização do código

--- Sessao2/exercicio1.py
+++ Sessao2/exercicio1.py
@@ -1,1 +1,9 @@
-print
+# scripts.py
+import sys
+print("Python:", sys.version)
+
+try:
+    import pip
+    print("pip:", pip.__version__)
+except Exception:
+    print("pip indisponível no ambiente.")
